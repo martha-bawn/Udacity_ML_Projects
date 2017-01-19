521c8b77a76e385111d8e56064edc3baaa3fcf57
Martha Bawn <marthalbawn@gmail.com>
2017-01-18 17:18:06 -0700

    Implement linear SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.svm import SVC
+clf = SVC(kernel = "linear")
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+print clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
+
+print clf.score(features_test, labels_test)
+
 #########################################################
 
 

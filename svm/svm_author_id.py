1a486e7782d5953cc36ffd7d4eb5311d8a4c9eb8
Martha Bawn <marthalbawn@gmail.com>
2017-01-18 17:26:40 -0700

    Change to rbf kernel and optimize C

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,8 +26,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 from sklearn.svm import SVC
-clf = SVC(kernel = "linear")
+clf = SVC(kernel = "rbf", C = 10)
+# C = 10 -> accuracy: 0.616040955631
+# C = 100 -> accuracy: 0.616040955631
+# C = 1000 -> accuracy: 0.821387940842
+# C = 10000 -> accuracy: 0.892491467577
 
+# use only 1% of training data
 features_train = features_train[:len(features_train)/100] 
 labels_train = labels_train[:len(labels_train)/100] 
 

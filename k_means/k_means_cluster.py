edea3e57368ae7a3fd692a6d5329ba58727ff6c1
Martha Bawn <marthalbawn@gmail.com>
2017-01-20 19:07:32 -0700

    Deploy k-means with 2 clusters

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -65,7 +65,12 @@ plt.show()
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+from sklearn.cluster import KMeans 
 
+cluster = KMeans(n_clusters=2)
+cluster.fit(finance_features)
+
+pred = cluster.predict(finance_features)
 
 
 ### rename the "name" parameter when you change the number of features

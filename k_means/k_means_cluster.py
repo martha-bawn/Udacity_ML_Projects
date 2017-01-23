b4cfc9f1d07bb61b9fcf7ee18406777dead71524
Martha Bawn <marthalbawn@gmail.com>
2017-01-23 14:34:22 -0700

    Add feature scaling

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,18 +48,33 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-feature_3 = "total_payments"
+# feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
+
+### feature scaling
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaled_features = scaler.fit_transform(finance_features)
+
+# get scaled value for salary = 200,000 and 
+# exercised_stock_options = 1,000,000
+features_test = numpy.append(finance_features, 
+    numpy.array([[200000., 1000000.]]), axis=0)
+scaled_features_test = scaler.fit_transform(features_test)
+
+print scaled_features_test[-1]
+    
+
 
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, _ in finance_features:
+for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 

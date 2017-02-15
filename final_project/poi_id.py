f44c84aa4340dba59c124e82f0e6363b2f965729
Martha Bawn <marthalbawn@gmail.com>
2017-02-15 14:19:14 -0700

    Update poi_id.py and tune AdaBoost

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -12,7 +12,7 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi','salary']
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -40,19 +40,45 @@ for person in data_dict:
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
+
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
+
+### Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaled_features = scaler.fit_transform(features)
 
 ### Task 4: Try a variety of classifiers
 ### Please name your classifier clf for easy export below.
 ### Note that if you want to do PCA or other multi-stage operations,
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
 
-# Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+### PCA
+from sklearn.decomposition import RandomizedPCA
+pca = RandomizedPCA(n_components=8, whiten=True).fit(features_train)
+pca.fit(features_train)
+pca_features_train = pca.transform(features_train)
+pca_features_test = pca.transform(features_test)
+# print pca.explained_variance_ratio_
+
+### AdaBoost
+from sklearn.ensemble import AdaBoostClassifier
+ab_clf = AdaBoostClassifier()
+ab_clf.fit(pca_features_train, labels_train)
+pred = ab_clf.predict(pca_features_test)
+# print "accuracy: ", clf.score(pca_features_test, labels_test)
+
+# print "precision: ", precision_score(labels_test, pred)
+# print "recall: ", recall_score(labels_test, pred)
+
+# print classification_report(labels_test, pred)
+
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -61,14 +87,23 @@ clf = GaussianNB()
 ### stratified shuffle split cross validation. For more info: 
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit
+# Tune AdaBoost
+parameters = {'n_estimators': (10, 20, 50, 70, 100)}
+sss = StratifiedShuffleSplit()
+
+clf = GridSearchCV(AdaBoostClassifier(), parameters, scoring="f1", cv=sss)
+clf.fit(pca_features_train, labels_train)
+
+print clf.best_params_
+print "F1: ", clf.best_score_
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
+
+from tester import test_classifier
+test_classifier(clf, my_dataset, features_list)
 
 dump_classifier_and_data(clf, my_dataset, features_list)
